4a2464ecd93e9ba2011a177bc8a157500ff1bb78
Frederic Fox <dev.frederic.fox@gmail.com>
2026-02-23 13:35:20 -0500

    build: :bug: base path

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: "export",
+	basePath: isProd ? "/portfolio-frederic-fox-winxp/" : "",
+	assetPrefix: isProd ? "/portfolio-frederic-fox-winxp/" : "",
 	images: { unoptimized: true },
 };
 
